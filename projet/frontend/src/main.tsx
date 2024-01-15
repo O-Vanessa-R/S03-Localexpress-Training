4be13c14e1907960d90673bb21ee227d69404a2b
Jeremy Dufroy <jeremy.dufroy.ext@oclock.io>
2024-01-15 10:23:37 +0100

    Add env variables local & prod

--- projet/frontend/src/main.tsx
+++ projet/frontend/src/main.tsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client'
 import App from './components/App';
 import './styles/main.scss';
 
+console.log(import.meta.env.VITE_API_URL);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
