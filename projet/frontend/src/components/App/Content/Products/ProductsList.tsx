fdae1b85fe3e1c7c4fc58c09b6df949514545fa4
Vanessa Rolland <vanessa.rolland@oclock.school>
2024-01-17 15:35:36 +0100

    beautiful with suspens

--- projet/frontend/src/components/App/Content/Products/ProductsList.tsx
+++ projet/frontend/src/components/App/Content/Products/ProductsList.tsx
@@ -1,18 +1,23 @@
 import { useState, useEffect } from "react";
 import fetchData, { delayedFetchData } from "../../../../api/fetchData";
 import { Product, ProductsData } from "../../../../@types/products";
+import groupByKey from "../../../../utils/groupBy";
+import ProductItem from "./ProductItem";
 
 const ressource = delayedFetchData("/products");
 
 const ProductsList = () => {
   ressource.read();
 
-  const [products, setProducts] = useState<Product[]>([]);
+  const [sortedProducts, setSortedProducts] = useState<
+    Record<string, Product[]>
+  >({});
 
   useEffect(() => {
     fetchData<ProductsData>("/products")
       .then((data) => {
-        setProducts(data.products);
+        const productsByCategory = groupByKey(data.products, "category");
+        setSortedProducts(productsByCategory);
       })
       .catch((error) => {
         console.error(error);
@@ -21,7 +26,15 @@ const ProductsList = () => {
 
   return (
     <div className="product-list">
-      <pre>{JSON.stringify(products, null, 4)}</pre>
+      {Object.entries(sortedProducts).map(([category, items]) => (
+        <section key={category} className="product-category">
+          <h2>{category}</h2>
+
+          {items.map((product) => (
+            <ProductItem key={product.id} product={product} />
+          ))}
+        </section>
+      ))}
     </div>
   );
 };
